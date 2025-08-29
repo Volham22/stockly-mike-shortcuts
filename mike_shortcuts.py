c681831918f386bee42bac090a30531446d904b3
Corentin Machu <corentin.machu@protonmail.com>
2025-08-29 17:28:03 +0200

    First solution
    Too slow

---
+++ mike_shortcuts.py
@@ -0,0 +1,46 @@
+def mike_shortcuts(n: int, shortcuts: list[int]) -> list[int]:
+    mins = [(2**32 - 1) for _ in range(n)]
+    mins[0] = 0
+    queue = [(0, 0)]
+
+    while len(queue) != 0:
+        current_intersection, consumed_energy = queue.pop(0)
+        mins[current_intersection] = min(mins[current_intersection], consumed_energy)
+
+        # Walk to next intersection
+        if (
+            current_intersection + 1 < n
+            and mins[current_intersection + 1] > consumed_energy + 1
+            and consumed_energy + 1 < n
+        ):
+            queue.append(
+                (
+                    current_intersection + 1,
+                    consumed_energy + 1,
+                )
+            )
+
+        # Use shortcuts
+        if mins[shortcuts[current_intersection] - 1] > consumed_energy + 1:
+            queue.append((shortcuts[current_intersection] - 1, consumed_energy + 1))
+
+        # Try to go back
+        if (
+            current_intersection - 1 > 0
+            and mins[current_intersection - 1] > consumed_energy + 1
+        ):
+            queue.append(
+                (
+                    current_intersection - 1,
+                    consumed_energy + 1,
+                )
+            )
+
+    return mins
+
+
+if __name__ == "__main__":
+    n = int(input())
+    shortcuts = [int(e) for e in input().split(" ")]
+    result = mike_shortcuts(n, shortcuts)
+    print(" ".join(str(n) for n in result))
